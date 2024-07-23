82efe2a142250af0b00178637805ff13b3871337
Jaime Castillo <escribeme@jaimelias.com>
2024-07-23 18:17:33 -0500

    removed unnecesary .toNumber conversion

--- test/test.js
+++ test/test.js
@@ -10,21 +10,21 @@ const TEST = async () => {
 
   indicators
     .init(unparsedOHLCV)
-    //.EMA(21)
-    //.EMA(9, 'ema21')
-    //.SMA(200)
-    //.SMA(100, 'sma_200')
+    .EMA(21)
+    .EMA(9, 'ema21')
+    .SMA(200)
+    .SMA(100, 'sma_200')
     .MACD(12, 26, 9)
     .BollingerBands(20, 2)
-    //.IchimokuCloud(9, 26, 52)
+    .IchimokuCloud(9, 26, 52)
     .RSI(14)
-    //.MFI(14)
-    //.RelativeVolume(10)
-    //.RelativeVolume(20)
+    .MFI(14)
+    .RelativeVolume(10)
+    .RelativeVolume(20)
     .crossPairs([{fast: 'ema_9', slow: 'ema_21'}, {fast: 'close', slow: 'sma_200'}, {fast: 'rsi_14', slow: 70}])
 
   console.log(indicators.getLastValues())
-  //console.log(VolumeProfile(BigNumber, indicators.getData(), 5))
+  console.log(VolumeProfile(BigNumber, indicators.getData(), 5))
 }
 
 TEST()
