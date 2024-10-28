bfbc2096585639e223c17096e9b53226b4915c3a
Jaime Castillo <escribeme@jaimelias.com>
2024-10-28 01:56:56 -0500

    added day_of_the_week, week_of_the_month, day_of_the_month

--- src/utilities/parsing-utilities.js
+++ src/utilities/parsing-utilities.js
@@ -32,6 +32,9 @@ export const parseOhlcvToVertical = (input, len) => {
     {
         verticalOhlcv['session_index'] = new Array(len)
         verticalOhlcv['session_intraday_index'] = new Array(len)
+        verticalOhlcv['day_of_the_week'] = new Array(len)
+        verticalOhlcv['day_of_the_month'] = new Array(len)
+        verticalOhlcv['week_of_the_month'] = new Array(len)
         prevDateStr = input[0].date.slice(0, 10)
     }
     
@@ -59,11 +62,43 @@ export const parseOhlcvToVertical = (input, len) => {
                 sessionIntradayIndex = 0
             }
 
+            const {
+                day_of_the_week,
+                day_of_the_month,
+                week_of_the_month
+            } = getDateInfo(thisDateStr)
+
+            verticalOhlcv.day_of_the_week[x] = day_of_the_week
+            verticalOhlcv.day_of_the_month[x] = day_of_the_month
+            verticalOhlcv.week_of_the_month[x] = week_of_the_month
             verticalOhlcv.session_index[x] = sessionIndex
             verticalOhlcv.session_intraday_index[x] = sessionIntradayIndex
+
             sessionIntradayIndex++
         }
     }
 
     return verticalOhlcv
 }
+
+
+const getDateInfo = dateString => {
+    const date = new Date(dateString);
+  
+    // Get day of the week (0 for Sunday, 1 for Monday, etc.)
+    const day_of_the_week = date.getDay()
+  
+    // Get day of the month
+    const day_of_the_month = date.getDate();
+  
+    // Calculate the week of the month
+    const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+    const dayOffset = firstDayOfMonth.getDay(); // Day of the week for the 1st day of the month
+    const week_of_the_month = Math.ceil((day_of_the_month + dayOffset) / 7);
+  
+    return {
+        day_of_the_week,
+        day_of_the_month,
+        week_of_the_month
+    };
+  }
