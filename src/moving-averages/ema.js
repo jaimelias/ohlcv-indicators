ca39fe16db36af4014a8ac9ec426ee4f18dea215
Jaime Castillo <escribeme@jaimelias.com>
2025-02-20 15:16:03 -0500

    fixed target selection in ema, and sma

--- src/moving-averages/ema.js
+++ src/moving-averages/ema.js
@@ -5,17 +5,25 @@ export const ema = (main, index, size, {target}) => {
 
   const {verticalOhlcv, instances, priceBased} = main
 
-  const value = verticalOhlcv.close[index]
 
   let prefix =  (typeof target === 'string' && verticalOhlcv.hasOwnProperty(target) && target !== 'close') ? `${target}_` : ''
   const keyName = `${prefix}ema_${size}`
 
   if(index === 0)
   {
+    const {nullArray} = main
+
+      if(!verticalOhlcv.hasOwnProperty(target))
+      {
+          throw new Error(`Target property ${target} not found in verticalOhlcv for ema.`)
+      }
+
       instances[keyName] = new FasterEMA(size)
-      verticalOhlcv[keyName] = [...main.nullArray]
+      verticalOhlcv[keyName] = [...nullArray]
       priceBased.push(keyName)
   }
+
+  const value = verticalOhlcv[target][index]
 
   let currEma
   instances[keyName].update(value, main.lastIndexReplace)
--- src/moving-averages/sma.js
+++ src/moving-averages/sma.js
@@ -4,7 +4,7 @@ import {FasterSMA} from 'trading-signals';
 export const sma = (main, index, size, {target}) => {
 
   const {verticalOhlcv, instances, priceBased} = main
-  const value = verticalOhlcv.close[index]
+  
 
   let prefix =  (typeof target === 'string' && verticalOhlcv.hasOwnProperty(target) && target !== 'close') ? `${target}_` : ''
   const keyName = `${prefix}sma_${size}`
@@ -12,11 +12,17 @@ export const sma = (main, index, size, {target}) => {
 
   if(index === 0)
   {
+      if(!verticalOhlcv.hasOwnProperty(target))
+      {
+          throw new Error(`Target property ${target} not found in verticalOhlcv for sma.`)
+      }
+
       instances[keyName] = new FasterSMA(size)
       verticalOhlcv[keyName] = [...main.nullArray]
       priceBased.push(keyName)
   }
 
+  const value = verticalOhlcv[target][index]
   let currSma
   instances[keyName].update(value, main.lastIndexReplace)
 
